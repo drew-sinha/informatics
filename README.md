cff4c0831538279dbe7e92886cbc0ffd61b2840b
DS <drew.b.sinha@gmail.com>
2026-03-23 16:45:24 -0400

    Initial commit, stable

---
+++ README.md
@@ -0,0 +1,9 @@
+Phenotyper
+
+Description: 
+`Phenotyper` is a program that converts ICD-10 or HPO terms into gene lists for genetic analysis
+
+All files current as of 26 Mar 2026, including,
+- ICD_phecode_map.csv: taken from phecode v1.2 provided with the phers package (https://github.com/hugheylab/phers)
+- HPO_phecode_map.csv: taken from phecode v1.2 provided with the phers package (https://github.com/hugheylab/phers)
+- phenotype_to_genes.txt: HPO/phenotype-to-gene associations provided publically by The Human Phenotype Ontology (HPO) (https://hpo.jax.org/data/annotations).
---
+++ generate_phenotypes.py
@@ -0,0 +1,53 @@
+import pathlib
+import csv
+import collections
+
+### Initialization code
+ICD_phecode_file = pathlib.Path("resources/ICD_phecode_map.csv")
+HPO_phecode_file = pathlib.Path("resources/HPO_phecode_map.csv")
+HPO_gene_file = pathlib.Path("resources/phenotype_to_genes.txt") #Note, this is actually a tsv file
+
+icd_phecode_map = {}
+icd_naming = {}
+phecode_naming = {}
+
+phecode_hpo_map = collections.defaultdict(list) #one-to-many map of hpo terms associated with a phecode
+hpo_naming = {}
+
+hpo_gene_map = collections.defaultdict(list) #one-to-many map of genes associated with an HPO term
+
+with ICD_phecode_file.open('r') as open_file:
+    file_reader = csv.reader(open_file)
+    file_reader.__next__() # skip header
+    for row in file_reader:
+        icd, _, icd_name, phecode, phecode_name = row
+        icd_phecode_map[icd] = phecode
+        icd_naming[icd] = icd_name
+        phecode_naming[phecode] = phecode_name
+
+with HPO_phecode_file.open('r') as open_file:
+    file_reader = csv.reader(open_file)
+    file_reader.__next__()
+    for row in file_reader:
+        hpo_id, hpo_name, phecode, _ = row
+        phecode_hpo_map[phecode].append(hpo_id)
+        hpo_naming[hpo_id] = hpo_name
+        
+with HPO_gene_file.open('r') as open_file:
+    file_reader = csv.reader(open_file, delimiter='\t')
+    file_reader.__next__()
+    for row in file_reader:
+        hpo_id, _, _, gene, _ = row
+        hpo_gene_map[hpo_id].append(gene)
+###
+
+def make_icd_gene_list(icd_code):
+    hpo_codes = phecode_hpo_map[icd_phecode_map[icd_code]]
+    genes = set([gene for hpo in hpo_codes for gene in hpo_gene_map[hpo]])
+    return genes
+
+def make_icd_hpo_list(icd_code):
+    return set(phecode_hpo_map[icd_phecode_map[icd_code]])
+    
+def make_hpo_gene_list(hpo_code)
+    return set(hpo_gene_map[hpo_code])
